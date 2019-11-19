2f5ebc17c24342d9d11f78411279048cc9b68d24
cpagano21 <christinapagano2020@u.northwestern.edu>
2019-11-19 15:46:40 -0600

    updated P6

--- P6/P6.js
+++ P6/P6.js
@@ -1,3 +1,7 @@
+function confirm() {
+  alert("You have finished. Thanks.");
+};
+
 function login() {
   var username = document.getElementById('username').value;
   var password = document.getElementById('password').value;
